a485476e3e8d337d96182f1ab82df05ae5769464
Joydeep Bhattacharjee <joydeepbhattacharjee.dits@gmail.com>
2025-09-13 19:20:26 +0530

    patient: Addinvestigation report api integration.

--- lib/core/network/apiHelper/api_endpoint.dart
+++ lib/core/network/apiHelper/api_endpoint.dart
@@ -10,7 +10,7 @@ class ApiEndPoint{
   ApiEndPoint._internal();
 
   //static const baseurl = "http://192.168.29.243:8001/api";
-//  static const baseurl = "https://devanthims.in/rainbow/api";
+// static const baseurl = "https://devanthims.in/rainbow/api";
  static const baseurl = "https://devanttest.in/kgh-demo/api";
  // static const baseurl = "https://jmnmedicalcollege.com/api";
 
--- lib/features/patient_module/investigation/data/investigation_usecases_impl.dart
+++ lib/features/patient_module/investigation/data/investigation_usecases_impl.dart
@@ -5,11 +5,10 @@ import 'package:jnm_hospital_app/core/network/apiHelper/resource.dart';
 import 'package:jnm_hospital_app/core/network/apiHelper/status.dart';
 import 'package:jnm_hospital_app/core/services/localStorage/shared_pref.dart';
 import 'package:jnm_hospital_app/features/patient_module/investigation/data/investigation_usecases.dart';
-import 'package:jnm_hospital_app/features/patient_module/model/investigation_report/investigation_report_model.dart';
-
+ 
 class InvestigationUsecasesImpl implements InvestigationUsecases {
   @override
-  Future<Resource> getInvestigationReport(InvestigationReportModel reportFilter) async {
+  Future<Resource> getInvestigationReport(Map<String, dynamic> reportFilter) async {
     final ApiClient _apiClient = getIt<ApiClient>();
     final SharedPref _pref = getIt<SharedPref>();
 
@@ -19,7 +18,7 @@ class InvestigationUsecasesImpl implements InvestigationUsecases {
     Resource resource = await _apiClient.postRequest(
         url: ApiEndPoint.getInvestigationReport,
         header: header,
-        requestData: {"from_date": "2025-08-01", "to_date": "2025-08-31"});
+        requestData: reportFilter);
     if (resource.status == STATUS.SUCCESS) {
       return resource;
     } else {
